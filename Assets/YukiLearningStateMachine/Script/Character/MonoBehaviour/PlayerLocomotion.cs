51062155cbb7479e4830edde7c5f2a3563839f24
Jiradechh <donminecraft36@gmail.com>
2026-07-22 19:17:36 +0700

    Try to add Rotate animation

--- Assets/YukiLearningStateMachine/Script/Character/MonoBehaviour/PlayerLocomotion.cs
+++ Assets/YukiLearningStateMachine/Script/Character/MonoBehaviour/PlayerLocomotion.cs
@@ -1,28 +1,42 @@
 using UnityEngine;
-
+[RequireComponent(typeof(CharacterController),typeof(Animator))]
 public class PlayerLocomotion : MonoBehaviour
 {
     [Header("Player Input")]
     [SerializeField] private YantraInputObserverSO _playerInput;
     private bool _enableUseInputObserverSO;
 
+    [Header("ReferencePoint")]
+    [SerializeField] private Transform _referencePoint;
+
+
+    [Header("Locomotion Animatiton Setting")]
+    [SerializeField] private float _dampTime = 0.1f;
+    private float _multiply;
+    [SerializeField] private string _nameParameterMoveZ;
+    [SerializeField] private string _nameParameterMoveX;
+    [SerializeField] private string _nameParameterTurn;
+
+    private int _moveZ;
+    private int _moveParameterX;
+    private int _turn;
+
     private CharacterController _characterController;
     private Animator _animator;
     private Vector3 _directionMove;
 
-    [Header("ReferencePoint")]
-    [SerializeField] private Transform _referencePoint;
-
-    
-    private bool _faceMoveDirection;
 
     void Awake()
     {
         _characterController = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
 
+        //Hash Sring
+        _moveZ = Animator.StringToHash(_nameParameterMoveZ);
+        _moveParameterX = Animator.StringToHash(_nameParameterMoveX);
+        _turn = Animator.StringToHash(_nameParameterTurn);
+
         _enableUseInputObserverSO = true;
-        _faceMoveDirection = false;
     }
 
     void OnEnable()
@@ -34,20 +48,37 @@ public class PlayerLocomotion : MonoBehaviour
     {
         _playerInput.OnMoveChannel -= HandleMoveInput;
     }
+    void Update()
+    {
+        // set Animation
+        Vector3 direction = GetDirectionWithReferencePoint();
+        Vector3 localVelocity = transform.InverseTransformDirection(direction);
+        float velocityZ = Mathf.Clamp(localVelocity.z, -1, 1);
+        float velocityX = Mathf.Clamp(localVelocity.x, -1, 1);
+        float turn = 0f;
+        if (direction.sqrMagnitude > 0.01f)
+        {
+            float angle = Vector3.SignedAngle(transform.forward, direction, Vector3.up);
+            turn = Mathf.Clamp(angle / 90f, -1f, 1f);
+        }
+
+
+
+        _animator.SetFloat(_moveZ, velocityZ * _multiply, _dampTime, Time.deltaTime);
+        _animator.SetFloat(_moveParameterX, velocityX * _multiply, _dampTime, Time.deltaTime);
+        _animator.SetFloat(_turn, turn, _dampTime, Time.deltaTime);
+    }
 
     void FixedUpdate()
     {
-
         bool hasMoveInput = _directionMove.sqrMagnitude > 0.01f;
         if (!hasMoveInput) return;
-        Rotate(_faceMoveDirection ?
-            GetWorldDirectionRelativeTo(_directionMove, _referencePoint) :
-            GetCameraForwardFlat());
+        //Rotate(GetCameraForwardFlat());
     }
     private void OnAnimatorMove()
     {
         _characterController.Move(_animator.deltaPosition);
-        
+        transform.rotation *= _animator.deltaRotation;
     }
 
     private void HandleMoveInput(Vector3 moveInput)
@@ -56,6 +87,9 @@ public class PlayerLocomotion : MonoBehaviour
         _directionMove = moveInput;
     }
 
+    
+
+    #region HelperMethod
     private Vector3 GetWorldDirectionRelativeTo(
     Vector3 inputDirection,
     Transform referenceTransform)
@@ -90,35 +124,23 @@ public class PlayerLocomotion : MonoBehaviour
 
         return flatForward.normalized;
     }
-
-    //for test
-    public void Rotate(Vector3 direction)
-    {
-        direction.y = 0f;
-
-        if (direction.sqrMagnitude < 0.0001f) return;
-
-        Quaternion targetRotation =
-            Quaternion.LookRotation(direction, Vector3.up);
-
-        Quaternion nextRotation = Quaternion.Slerp(transform.rotation,
-            targetRotation, Time.deltaTime);
-
-        transform.rotation = targetRotation;
-    }
+    #endregion
 
     #region //API
 
     // Enable function
     public void SetEnableUseInputObserverSO(bool enable) => _enableUseInputObserverSO = enable;
     public bool GetEnableUseInputObserverSO() => _enableUseInputObserverSO;
-    public void SetEnableFaceMoveDirection(bool enable) => _faceMoveDirection = enable;
-    public bool SetEnableFaceMoveDirection() => _faceMoveDirection;
 
     // Direction
     public void SetDirection(Vector3 direction) => _directionMove = direction;
     public Vector3 GetDirection() => _directionMove;
     public Vector3 GetDirectionWithReferencePoint() => GetWorldDirectionRelativeTo(_directionMove,_referencePoint);
+
+    //Animaiton
+    // Get,Set Mutiply
+    public float GetMutiply() => _multiply;
+    public void  SetMuitply(float multiply) => _multiply = multiply;
 
     #endregion
 }
--- Assets/YukiLearningStateMachine/Script/Character/Statemachine/Action/PlayerLocomotionAnimationActionSO.cs
+++ Assets/YukiLearningStateMachine/Script/Character/Statemachine/Action/PlayerLocomotionAnimationActionSO.cs
@@ -7,68 +7,32 @@ menuName = "YUKI Learning State Machine/Actions/Player/PlayerLocomotionAnimation
 public class PlayerLocomotionAnimationActionSO : StateActionSO
 {
     [Header("Animation Setting")]
-    public float DampTime;
     public float Multiply;
-
-    public LocomotionAnimationParameter locomotionAnimationParameter;
 
     public override StateAction CreateAction(StateMachine stateMachine)
     {
-        return new PlayerLocomotionAnimationAction(DampTime,Multiply ,locomotionAnimationParameter);
+        return new PlayerLocomotionAnimationAction(Multiply);
     }
 }
 
 public class PlayerLocomotionAnimationAction : StateAction
 {
-    private Animator _animator; 
     private PlayerLocomotion _playerLocomotion;
-    private Transform _transform;
-    
-    private float _dampTime;
     private float _multiply;
 
-    private string _animationStateName;
-
-    private string _nameparameterZ;
-    private string _nameparameterx;
-
-    
-
-    public PlayerLocomotionAnimationAction(float dampTime, float multiply, LocomotionAnimationParameter locomotionAnimationParameter)
+    public PlayerLocomotionAnimationAction(float multiply)
     {
-
-        _dampTime = dampTime;
         _multiply = multiply;
-
-        _animationStateName = locomotionAnimationParameter.AnimationStateName;
-
-        _nameparameterZ = locomotionAnimationParameter.NameparameterZ;
-        _nameparameterx = locomotionAnimationParameter.NameparameterX;
     }
 
     public override void Awake(StateMachine stateMachine)
     {
-         _animator = stateMachine.GetComponent<Animator>();
          _playerLocomotion = stateMachine.GetComponent<PlayerLocomotion>();
-         _transform = stateMachine.GetComponent<Transform>();
     }
-
-    public override void OnUpdate()
+    public override void OnStateEnter()
     {
-        
-        Vector3 localVelocity = _transform.InverseTransformDirection(_playerLocomotion.GetDirectionWithReferencePoint());
-        float velocityZ = Mathf.Clamp(localVelocity.z, -1, 1);
-        float velocityX = Mathf.Clamp(localVelocity.x, -1, 1);
-        _animator.SetFloat(_nameparameterZ, velocityZ * _multiply, _dampTime, Time.deltaTime);
-        _animator.SetFloat(_nameparameterx, velocityX * _multiply, _dampTime, Time.deltaTime);
+        _playerLocomotion.SetMuitply(_multiply);
     }
-}
 
-[System.Serializable]
-public struct LocomotionAnimationParameter
-{
-    public string AnimationStateName;
-
-    public string NameparameterZ;
-    public string NameparameterX;
+    public override void OnUpdate(){}
 }
