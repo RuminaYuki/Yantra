074cfb312c0dda0be8ddc8bc504ae7c94344f5f3
RuminaYuki <donminecraft36@gmail.com>
2026-07-31 00:55:42 +0700

    setup folder again

--- Assets/YukiLearningStateMachine/Script/StateMachineCore/Anchor/Provider/TransformProvider.cs
+++ Assets/YukiLearningStateMachine/Script/StateMachineCore/Anchor/Provider/TransformProvider.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class TransformProvider : MonoBehaviour
 {
-    [SerializeField]private TransformAnchor _transformAnchor;
+    [SerializeField] private TransformAnchor _transformAnchor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
