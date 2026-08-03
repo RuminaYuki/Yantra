235694adda7a88e441c58405c0e6909804a4dfa0
RuminaYuki <donminecraft36@gmail.com>
2026-08-04 01:10:10 +0700

    add pathfinder core

---
+++ Assets/Scripts/Character/General/LocomotionSystem/PathNavigator.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PathNavigator : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _repathInterval = 0.25f;
+
+    private IPathfinder _pathfinder;
+    private float _timer;
+
+    public Vector3 Direction { get; private set; }
+
+    private void Awake()
+    {
+        _pathfinder = new UnityNavMeshPathfinder();
+    }
+
+    private void Update()
+    {
+        if (_target == null)
+        {
+            Direction = Vector3.zero;
+            return;
+        }
+
+        _timer -= Time.deltaTime;
+
+        if (_timer <= 0f)
+        {
+            _timer = _repathInterval;
+
+            _pathfinder.CalculatePath(
+                transform.position,
+                _target.position
+            );
+        }
+
+        Direction = _pathfinder.GetDirection(transform.position);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        _target = target;
+    }
+}
